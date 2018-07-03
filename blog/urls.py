7ebaaf4c53366f2ec23ae752749ba30d65d17ce2
bhuvana.kundumani <bhuvana.kundumani@gmail.com>
2018-07-03 22:13:39 +0530

    final changes commited

---
+++ blog/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import url
+from . import views
+from django.urls import include, path
+
+
+
+urlpatterns = [
+
+    path('', views.post_list, name='post_list'),
+    #path('post/(?P<pk>\d+)/', views.post_detail, name='post_detail'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new', views.post_new, name='post_new')
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+
+    #re_path('post/(?P<pk>\d+)/', views.post_detail, name='post_detail'),
+    #re_path('post/new/', views.post_new, name='post_new'),
+    #re_path('post/(?P<pk>\d+)/edit/', views.post_edit, name='post_edit'),
+
+]
